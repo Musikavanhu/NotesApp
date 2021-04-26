e0e94fbb0d4d93a2e0797d4a404060d50a2a5ed2
Tinotenda Musikavanhu <thesafehavenhero@gmail.com>
2021-04-26 14:27:53 -0500

    Add existing file

--- server.js
+++ server.js
@@ -3,24 +3,21 @@ var path = require("path");
 var fs = require('fs');
 var util = require('util');
 
-// Tells node that we are creating an "express" server
+
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+
 var PORT = process.env.PORT || 8000;
 
-// Sets up the Express app to handle data parsing
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "./public")));
 
-//Set variables
 const writefileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 let allNotes;
 
-// ROUTER
-// The below points our server to set up routes
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
@@ -41,19 +38,19 @@ app.post("/api/notes", function (req, res) {
     readFileAsync(path.join(__dirname, "./db/db.json"), "utf8")
         .then(function (data) {
             allNotes = JSON.parse(data);
-            if (newNote.id || newNote.id === 0) {
-                let currNote = allNotes[newNote.id];
-                currNote.title = newNote.title;
-                currNote.text = newNote.text;
+  if (newNote.id || newNote.id === 0) {
+   let currNote = allNotes[newNote.id];
+   currNote.title = newNote.title;
+   currNote.text = newNote.text;
             } else {
                 allNotes.push(newNote);
             }
-            writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
+   writefileAsync(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes))
                 .then(function () {
                     console.log("Wrote db.json");
                 })
         });
-    res.json(newNote);
+res.json(newNote);
 });
 
 app.delete("/api/notes/:id", function (req, res) {
@@ -69,9 +66,6 @@ app.delete("/api/notes/:id", function (req, res) {
         });
     res.json(id);
 });
-
-// LISTENER
-// The below code effectively "starts" our server
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
